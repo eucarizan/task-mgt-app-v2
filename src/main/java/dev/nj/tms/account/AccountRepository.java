b3bd989ce897d160a99dfc33ae88f335d58b9a94
eucarizan <eucarizan@gmail.com>
2025-08-12 20:38:47 +0800

    fix(account): correct generic order in CrudRepository<Account, Long>

--- src/main/java/dev/nj/tms/account/AccountRepository.java
+++ src/main/java/dev/nj/tms/account/AccountRepository.java
@@ -1,8 +1,10 @@
 package dev.nj.tms.account;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-public interface AccountRepository extends CrudRepository<Long, Account> {
+@Repository
+public interface AccountRepository extends CrudRepository<Account, Long> {
     boolean existsByEmailIgnoreCase(String email);
     Account save(Account account);
 }
