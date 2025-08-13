e453e2e2d914e356a837e37960a1d8de334b41fd
eucarizan <eucarizan@gmail.com>
2025-08-13 12:54:32 +0800

    test(task): add failing test for authenticated task listing (200)

---
+++ src/test/java/dev/nj/tms/task/TaskControllerTest.java
@@ -0,0 +1,26 @@
+package dev.nj.tms.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(TaskController.class)
+public class TaskControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private TaskService taskService;
+
+    @Test
+    void shouldReturn200WhenAuthenticatedUserRequestsTasks() throws Exception {
+        mockMvc.perform(get("/api/tasks"))
+                .andExpect(status().isOk());
+    }
+}
