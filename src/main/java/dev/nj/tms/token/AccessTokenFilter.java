2b9f949809600fa0b4ffdcb3c395c88e598ccce3
eucarizan <eucarizan@gmail.com>
2025-10-14 16:41:40 +0800

    refactor: accesstokenfilter - added selective endpoint filtering

--- src/main/java/dev/nj/tms/token/AccessTokenFilter.java
+++ src/main/java/dev/nj/tms/token/AccessTokenFilter.java
@@ -7,32 +7,41 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.server.PathContainer;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.pattern.PathPattern;
+import org.springframework.web.util.pattern.PathPatternParser;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AccessTokenFilter extends OncePerRequestFilter {
 
     private static final Logger logger = LoggerFactory.getLogger(AccessTokenFilter.class);
 
     private final AuthenticationManager authenticationManager;
+    private final List<EndpointMatcher> matchers = new ArrayList<>();
+    private final PathPatternParser parser = new PathPatternParser();
 
     public AccessTokenFilter(AuthenticationManager authenticationManager) {
         this.authenticationManager = authenticationManager;
+
+        addRequestMatcher("/api/tasks", HttpMethod.GET);
+        addRequestMatcher("/api/tasks", HttpMethod.POST);
     }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String authHeader = request.getHeader("Authorization");
 
-        if (authHeader != null && authHeader.startsWith("Bearer ")) {
-            String token = authHeader.substring(7);
-            logger.info("Extracted bearer token: {}", token );
-
+        if (requiredBearerAuth(request)) {
+            String token = extractBearerToken(request);
             try {
                 BearerTokenAuthenticationToken authRequest = new BearerTokenAuthenticationToken(token);
                 Authentication authenticated = authenticationManager.authenticate(authRequest);
@@ -41,8 +50,51 @@ public class AccessTokenFilter extends OncePerRequestFilter {
             } catch (AuthenticationException e) {
                 logger.debug("Authentication failed: {}", e.getMessage());
                 SecurityContextHolder.clearContext();
+                return;
             }
         }
         filterChain.doFilter(request, response);
+    }
+
+    private boolean requiredBearerAuth(HttpServletRequest request) {
+        String method = request.getMethod();
+        String path = request.getRequestURI();
+
+        return matchers.stream().anyMatch(matcher ->
+                matcher.method.name().equals(method) && matcher.pattern.matches((PathContainer) PathPatternParser.defaultInstance.parse(path))
+        );
+    }
+
+    private String extractBearerToken(HttpServletRequest request) {
+        String authHeader = request.getHeader("Authorization");
+
+        if (authHeader == null || authHeader.isBlank()) {
+            throw new BadCredentialsException("Missing Authorization header");
+        }
+
+        String[] parts = authHeader.split("\\s+");
+
+        if (parts.length != 2) {
+            throw new BadCredentialsException("Malformed authorization header. Expected format: 'Bearer <token')");
+        }
+
+        if (!"Bearer".equalsIgnoreCase(parts[0])) {
+            throw new BadCredentialsException("Authorization header must start with 'Bearer'");
+        }
+
+        String token = parts[1].trim();
+        if (token.isEmpty()) {
+            throw new BadCredentialsException("Bearer token cannot be empty");
+        }
+
+        logger.info("Extracted bearer token: {}", token);
+    }
+
+    private void addRequestMatcher(String pattern, HttpMethod httpMethod) {
+        this.matchers.add(new EndpointMatcher(parser.parse(pattern), httpMethod));
+    }
+
+    private record EndpointMatcher(PathPattern pattern, HttpMethod method) {
+
     }
 }
