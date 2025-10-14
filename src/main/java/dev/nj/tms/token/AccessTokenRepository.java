6a6623a3885ff438eea484e0a79dc85e1ce67d07
eucarizan <eucarizan@gmail.com>
2025-10-14 13:36:06 +0800

    updated access token repository

--- src/main/java/dev/nj/tms/token/AccessTokenRepository.java
+++ src/main/java/dev/nj/tms/token/AccessTokenRepository.java
@@ -3,6 +3,12 @@ package dev.nj.tms.token;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 @Repository
 public interface AccessTokenRepository extends CrudRepository<AccessToken, Long> {
+    Optional<AccessToken> findByToken(String token);
+
+    int deleteByExpiresAtBefore(LocalDateTime dateTime);
 }
